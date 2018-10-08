82e828bfbf9a83f3e1de9e7d6f2d238b9cf8bb1e
anderejd <rajder@gmail.com>
2018-10-08 14:53:57 +0200

    Improved profiling scripts.

--- profile_perf.sh
+++ profile_perf.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Based on https://gist.github.com/KodrAus/97c92c07a90b1fdd6853654357fd557a
-sudo perf record --call-graph dwarf target/release/examples/subdiv_benchmark
+# Make sure to uncomment the debug flag under [profile.release] in Cargo.toml.
+# Needs https://github.com/brendangregg/FlameGraph at ../FlameGraph/
+cargo build --release --example subdiv_benchmark
+sudo perf record --call-graph dwarf -F 10000 target/release/examples/subdiv_benchmark
 sudo chown $USER perf.data
 perf script | ../FlameGraph/stackcollapse-perf.pl | ../FlameGraph/flamegraph.pl > flame.svg
 
